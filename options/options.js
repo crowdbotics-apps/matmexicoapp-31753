46e5c7df3ab1e580a503672d6755393d148b3324
Crowdbotics <team@crowdbotics.com>
2021-10-29 21:04:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "matmexicoapp_31753",
+  url: "https://matmexicoapp_31753.botics.co",
+  api: "https://matmexicoapp_31753.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
